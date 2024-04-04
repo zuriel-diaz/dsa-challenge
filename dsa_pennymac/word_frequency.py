2c8dc0cd1626cadec84e5b403039970ec1aed35d
Zuriel Diaz <zuriel_diaz@epam.com>
2024-04-03 22:09:14 -0600

    Identify single occurrence and base cases.

--- dsa_pennymac/word_frequency.py
+++ dsa_pennymac/word_frequency.py
@@ -6,8 +6,22 @@ that outputs how many times you would be able to spell
 If you are unable to spell it, please return 0.
 """
 
+from collections import Counter
 from typing import Union
 
 
 def solution(word: str, letters: Union[list, str]) -> int:
-    return 0
+    if len(letters) == 0:
+        return 0
+
+    letter_frequency, occurrence_number = Counter(word), 0
+
+    for letter in letter_frequency:
+        if letter in letters:
+            if letter_frequency[letter] != letters.count(letter):
+                return occurrence_number
+        else:
+            return occurrence_number
+
+    occurrence_number += 1
+    return occurrence_number
